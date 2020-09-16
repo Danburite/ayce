660609fa436f2110d4620f11a99b5dd38fb46bef
Sherman Lee <Galerider1@live.com>
2020-09-15 21:38:43 -0700

    Made controller pages more dynamic with multiple pages of the same type

--- ayce-main/ayce-code/Controllers/ButterCMSPageController.cs
+++ ayce-main/ayce-code/Controllers/ButterCMSPageController.cs
@@ -14,30 +14,32 @@ namespace ButterCMSPage.Controllers
     {
         public ActionResult Main()
         {
-            ButterCMSClientHandler<Pages.MainPage> mainhandler = new ButterCMSClientHandler<Pages.MainPage>();
+            ButterCMSClientHandler<Pages.MainPage> mainhandler = new ButterCMSClientHandler<Pages.MainPage>("main_page");
 
             var mainModel = new Pages.MainPage();
             //Modify slugs on CMS to remove - delimiter and change components to accomodate
-            mainModel.copyData(mainhandler.classType);
+            var mainPageList = mainhandler.classType.Data.ToList();
+            
+            mainModel.copyData(mainPageList[0]);
 
             return View(mainModel);
         }
 
         public ActionResult Dish()
         {
-
+            
             return View();
         }
 
         public class ButterCMSClientHandler<className> where className : class, new()
         {
-            public ButterCMSClientHandler()
+            public ButterCMSClientHandler(string slug)
             {
                 className tempClass = new className();
                 var butterClient = new ButterCMSClient("d185149ebfab8b4ca09538b4bb7fd3c2f7b0a80f");
-                classType = new ButterCMS.Models.PageResponse<className>();
+                classType = new ButterCMS.Models.PagesResponse<className>();
                 //Dynamically change the "main" to page views
-                classType = butterClient.RetrievePage<className>("*", "main");
+                classType = butterClient.ListPages<className>(slug, null);
             }
 
             //Implement for component plugs
@@ -72,7 +74,7 @@ namespace ButterCMSPage.Controllers
                 return temp.ToString();
             }
 
-            public ButterCMS.Models.PageResponse<className> classType { get; set; }
+            public ButterCMS.Models.PagesResponse<className> classType { get; set; }
 
         }
     }
